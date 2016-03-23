62b64c189ad4961bf87e5e16d175ef5bfdba2c0b
Chris Mytton <chrismytton@gmail.com>
2016-03-23 14:39:34 +0100

    Add basic API for creating a site

--- app.rb
+++ app.rb
@@ -4,31 +4,32 @@ Dotenv.load
 
 require 'pry'
 require 'octokit'
+require 'sinatra'
 
 GITHUB_ORGANIZATION = ENV.fetch('GITHUB_ORGANIZATION', 'theyworkforyou')
-REPO_NAME = ARGV.first
 
-# This uses OCTOKIT_ACCESS_TOKEN from ENV to auth with GitHub.
-repo = Octokit.create_repository(REPO_NAME, organization: GITHUB_ORGANIZATION)
+post '/sites' do
+  # This uses OCTOKIT_ACCESS_TOKEN from ENV to auth with GitHub.
+  repo = Octokit.create_repository(params[:name], organization: GITHUB_ORGANIZATION)
+  Octokit.create_issue(repo[:full_name], 'Non-technical tasks', erb(:non_technical_tasks))
+  Octokit.create_issue(repo[:full_name], 'Technical tasks', erb(:technical_tasks))
+  repo[:html_url]
+end
 
-NON_TECHNICAL_TASKS = <<-NTT.freeze
+__END__
+
+@@ non_technical_tasks
 - [ ] Gather any non-legislative data that's needed in CSV format
 - [ ] Decide on a domain name for the site
 - [ ] Get Twitter username
 - [ ] Get email address
 - [ ] Setup Google Analytics
-NTT
 
-Octokit.create_issue(repo[:full_name], 'Non-technical tasks', NON_TECHNICAL_TASKS)
-
-TECHNICAL_TASKS = <<-TT.freeze
-- [ ] Push an unstyled theme to the new repo
-- [ ] Create an [orphan `gh-pages` branch](https://help.github.com/articles/creating-project-pages-manually/#create-a-gh-pages-branch) for building the site into
+@@ technical_tasks
+- [ ] Add an unstyled theme to this repo
 - [ ] Include a `404.html` template
-- [ ] Configure the site to build on Travis CI
+- [ ] Create an [orphan `gh-pages` branch](https://help.github.com/articles/creating-project-pages-manually/#create-a-gh-pages-branch) for building the site into
+- [ ] Configure this repo to build on Travis CI
 - [ ] Add required plugins and configure them
 - [ ] Add this country to the datasource updater
 - [ ] Setup DNS
-TT
-
-Octokit.create_issue(repo[:full_name], 'Technical tasks', TECHNICAL_TASKS)
